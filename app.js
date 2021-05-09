e033ed1a7678fe881b6db4648daa6328353127cf
Thi Le <thilehy7865@gmail.com>
2021-05-09 17:32:01 +0700

    test

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const app = express();
-const PORT = 4201;
+const PORT = process.env.PORT || 4201;
 
-const client = require('./config/db');
+// const client = require('./config/db');
 
-const url = process.env.MONGODB_URI;
-// const url = 'mongodb+srv://dara-restaurant:Thi786569@cluster0.2yk3t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+// const url = process.env.MONGODB_URI;
+// const url = 'mongodb+srv://dara-restaurant:Thi786569@cluster0.2yk3t.mongodb.net/Cluster0?retryWrites=true&w=majority';
 
 app.use(express.json());
-
-const favicon = require("express-favicon");
-app.use(favicon(__dirname + '/public/favicon.png'));
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
@@ -21,15 +18,19 @@ app.use((req, res, next) => {
 
 app.get('/', (req, res) => { res.send("hello this is app.get()") });
 
-client.connect(url, (err) => {
-    if (err) {
-        console.log("Unable to connect to mongodb");
-        process.exit();
-    } else {
-        app.listen(process.env.PORT || PORT, (err) => {
-            console.log(`Listening to localhost: ${PORT}`)
-        })
-        const control_controllers = require('./controllers/control_controllers');
-        app.use('/', control_controllers);
-    }
-});
+app.listen(PORT, () => {
+    console.log(`Listening to PORT: ${PORT}`)
+})
+
+// client.connect(url, (err) => {
+//     if (err) {
+//         console.log("Unable to connect to mongodb");
+//         process.exit();
+//     } else {
+//         app.listen(process.env.PORT || PORT, (err) => {
+//             console.log(`Listening to localhost: ${PORT}`)
+//         })
+//         const control_controllers = require('./controllers/control_controllers');
+//         app.use('/', control_controllers);
+//     }
+// });
