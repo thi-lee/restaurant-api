8d0311559c8f0f61b7f70987380cfe13fbca3426
Thi Le <thilehy7865@gmail.com>
2021-05-11 17:20:43 +0700

    update PORT

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 4201;
+const PORT = process.env.PORT || 4201;
 
 require('dotenv').config();
 const client = require('./config/db');
