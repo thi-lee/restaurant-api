0ac5c2a1e6385d2a14d73d1c592886704dbdc08a
Thi Le <thilehy7865@gmail.com>
2021-05-20 20:22:03 +0700

    modified access-control-allow-methods

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Credentials", "true")
-    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+    res.header("Access-Control-Allow-Origin", "https://dara-restaurant.netlify.app/");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST, PUT");
     res.header("Access-Control-Allow-Headers", "*");
     next();
